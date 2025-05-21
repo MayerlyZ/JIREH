b7c46206232ce57310d0a01d9cc71cf0006a1983
Mayerly Zapata <mayerlyzapatarodriguez@gmail.com>
2025-05-20 19:22:29 -0500

    commit

--- demo/demo/src/main/java/com/hotel/demo/repository/clienterepository.java
+++
@@ -1,9 +0,0 @@
-package com.hotel.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hotel.demo.model.cliente;
-
-public interface clienterepository extends JpaRepository<cliente, Long> {
-}
-
--- demo/demo/src/main/java/com/hotel/demo/repository/serviciosrepository.java
+++
@@ -1,7 +0,0 @@
-package com.hotel.demo.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.hotel.demo.model.servicios;
-
-public interface serviciosrepository extends JpaRepository<servicios, Long> {
-}
-
